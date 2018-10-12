8a165aad9e84d6d0b1224c74bdf1d39be67e6231
Megija <megi0005@edu.easj.dk>
2018-10-12 10:17:34 +0200

    final version

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,30 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let inputElement: HTMLInputElement= <HTMLInputElement>document.getElementById("weightInput");
+
+let ouncesButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ouncesButton");
+ouncesButton.addEventListener("click", ounces);
+
+let gramsButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("gramsButton");
+gramsButton.addEventListener("click", grams);
+
+let outputElement: HTMLOutputElement = <HTMLOutputElement>document.getElementById("weightOutput");
+
+
+function ounces() :void
+{
+    let inputWeight: string =inputElement.value;
+    let conversion: number = (parseInt(inputWeight)  * 0.0352739619);
+    outputElement.innerHTML = conversion.toString();
+
+}
+ 
+function grams() :void
+{
+    let inputWeight: string = inputElement.value;
+    let conversion: number = (parseInt(inputWeight) * 28.3495231);
+    outputElement.innerHTML = conversion.toString();
+
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+
+
